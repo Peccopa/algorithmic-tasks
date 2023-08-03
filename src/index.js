0698bbc85516991027702a1b677a498810d9b6c4
Andrey Zharkikh <orbital.logistics@yandex.ru>
2023-08-03 10:32:13 +0300

    task: complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 module.exports = function reverse (n) {
-  
+    let result = '';
+    n = String(Math.abs(n));
+    for (let i = n.length - 1; i >= 0; i--) {
+        result += n[i];
+    } return result;
 }
