e3916a4b56933e786df5d57ca69ffca1e4ba1ed0
Andrey Zharkikh <orbital.logistics@yandex.ru>
2023-10-24 11:39:32 +0300

    init: task complete

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,25 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.min;
+        this.max;
+    }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        this.num = Math.ceil(this.min + (this.max - this.min) / 2);
+        return this.num;
     }
 
     lower() {
-
+        this.setRange(this.min, this.num);
     }
 
     greater() {
-
+        this.setRange(this.num, this.max);
     }
 }
 
